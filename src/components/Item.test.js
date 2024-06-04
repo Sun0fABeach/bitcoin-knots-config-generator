403b728a15ae9335c86387ac053536816b58a717
Markus Appel <markospoparkos@gmail.com>
2024-06-04 15:57:08 +0200

    remove tests

--- src/components/Item.test.js
+++
@@ -1,9 +0,0 @@
-/* global it */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Item from './Section';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Item title='Section one' description='Defines some stuff.'><h1>Hello!</h1></Item>, div);
-});
