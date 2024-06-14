167d3a6ac02dccae8c004e67fb7c7b1fde66127f
Markus Appel <markospoparkos@gmail.com>
2024-06-14 16:00:57 +0200

    responsive header with icon

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import logo from '../knots_logo.svg'
+import './TopBar.css';
+
+const version = '26.x'
 
 function TopBar () {
   return (
     <div className='mdl-layout__header'>
-      <div className='mdl-layout__header-row'>
-        <span className='mdl-layout-title'>Bitcoin Knots Config Generator (compatible with Bitcoin Knots 26.x)</span>
+      <div className='mdl-layout__header-row top-bar'>
+        <img src={logo} alt="Knots logo" width="50" height="50" />
+        <div className='mdl-layout-spacer' />
+        <span className='mdl-layout-title title-long'>Bitcoin Knots Config Generator (compatible with Knots {version})</span>
+        <span className='mdl-layout-title title-short'>Config Generator (Knots {version})</span>
         <div className='mdl-layout-spacer' />
       </div>
     </div>
